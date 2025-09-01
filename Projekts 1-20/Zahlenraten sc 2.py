918a35904aae7dc74e7f60627278cd83fdab79e6
Leranias <151139865+Leranias@users.noreply.github.com>
2025-09-01 16:24:15 +0200

    ich muss drüber nach grübeln xD

--- Projekts 1-20/Zahlenraten sc 2.py
+++ Projekts 1-20/Zahlenraten sc 2.py
@@ -7,4 +7,35 @@ import random
 
 
 
-while True
+while True:
+
+
+
+    versuche = 3
+
+    while versuche > 1:
+        try:
+            print("willkomen beim zahlen raten")
+            spielModusAuswahl = input("willst du selber wählen wie hoch du raten willst ? ja/nein").lower()
+
+            if spielModusAuswahl =="ja":
+                unterezahl = int("gib die niedrigste oder kleinste zahl ein ")
+                höchstezahl =int("gib nun die höchste zahl oder das maximum ein ")
+                zahlenraten(unterezahl,höchstezahl)
+
+
+
+                
+            
+
+
+        
+
+
+
+    def zahlenraten(min,max):
+       randomzahl = random.randint(min,max)
+        
+       
+    
+    
