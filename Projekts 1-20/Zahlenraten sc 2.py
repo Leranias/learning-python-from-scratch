e8db0b11280849978e8a1566c1a4d340cbb3bd75
Leranias <151139865+Leranias@users.noreply.github.com>
2025-09-01 17:23:29 +0200

    script 2 zahlenraten Fertig

--- Projekts 1-20/Zahlenraten sc 2.py
+++ Projekts 1-20/Zahlenraten sc 2.py
@@ -2,40 +2,66 @@ import random
 
 
 
+#funktion die das zahlen raten darstellt
+#ich wollte sie eigendlich unten in code platzieren aber 
+#wollte python nicht weil unnereichbar was sinn macht aber
+#aus c# bin ich das etwas anders gewohnt
 
-
-
-
-
-while True:
-
-
-
+def zahlenraten(min,max):
+    
+    #hier holen wir uns die übergebenen parameter und nutzen sie als min und max wert 
+    randomzahl = random.randint(min,max)    
     versuche = 3
-
-    while versuche > 1:
+    #spiele schleife
+    while versuche >0:
         try:
-            print("willkomen beim zahlen raten")
-            spielModusAuswahl = input("willst du selber wählen wie hoch du raten willst ? ja/nein").lower()
-
-            if spielModusAuswahl =="ja":
-                unterezahl = int("gib die niedrigste oder kleinste zahl ein ")
-                höchstezahl =int("gib nun die höchste zahl oder das maximum ein ")
-                zahlenraten(unterezahl,höchstezahl)
-
-
-
-                
+            gerateneZahl = int(input("dann Rate mal los bitte deine zahl eingeben: = "))
             
+            # das eigendliche spiel beginnt hier
+            if gerateneZahl == randomzahl:
+                print(f"WOW du hast die zahl erraten deine zahl war {randomzahl} un glaublich!! ")
+                break
+            
+            elif gerateneZahl < randomzahl:
+                print("Zu Klein !")
+                versuche -= 1
+            elif gerateneZahl > randomzahl:
+                print("Zu Hoch! ")
+                versuche -= 1
+            else:
+                print("öö wie zum teufel  bist du hier hin gekommen ? husch ins körpchen ")
+                break
+
+        except ValueError:
+            print("bitte gib eine zahl an wird neugestartet ")
+            break
 
 
-        
-
-
-
-    def zahlenraten(min,max):
-       randomzahl = random.randint(min,max)
-        
-       
+#Hauptanwendungs code
+while True:
+    #fall back und standart zahlen zum raten
+    unterezahl = 1
+    höchstezahl = 10
     
-    
+    try:
+        #auswahl ob man selber die zahlen wählen zu können
+        print("willkomen beim zahlen raten")
+        spiel_Modus_Auswahl_Zahlenraten = input("willst du selber wählen wie hoch du raten willst? ja/nein: ").lower()
+
+        if spiel_Modus_Auswahl_Zahlenraten == "ja":
+            unterezahl = int(input("gib die niedrigste oder kleinste zahl ein "))
+            höchstezahl = int(input("gib nun die höchste zahl oder das maximum ein "))
+            zahlenraten(unterezahl,höchstezahl)
+            
+        else:
+            
+            print("standart also gut dann 1 bis 10")
+            zahlenraten(unterezahl,höchstezahl)
+    except TypeError:
+        print("fehler")
+
+
+    print("SPIEL ENDE!!!")
+    auswahl = input("willst du weiter spielen ja/nein ").lower()
+    if auswahl =="nein":
+        break
