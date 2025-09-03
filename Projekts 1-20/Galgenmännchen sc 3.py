0a7bb936e021b28871f5846d9c462aefa961f028
Leranias <151139865+Leranias@users.noreply.github.com>
2025-09-03 11:23:40 +0200

    Script fertig mit grübeln über verbesserungen

--- Projekts 1-20/Galgenmännchen sc 3.py
+++ Projekts 1-20/Galgenmännchen sc 3.py
@@ -41,4 +41,36 @@ while aktuelle_fehler <max_fehler and "-" in anzeige:
     print(f"fehler: {aktuelle_fehler} von {max_fehler} maximalen fehlern ")
 
     eingabe = input("Rate einen Buchstaben: ").lower()
-    
+    #hier wird geschaut ob die länge der eingabe des nutzers auch wirklich einen einzigen buchstaben umfasst 
+    #man könnte hier auch sagen oder prüfen ob das eingegebene wort das ziel wort ist oder nicht wenn nicht fehler
+    #hier kam mir die idee eine prüfung einzubauen sowas wie
+    # if eingabe == geheimes_wort: um den fall abzufrühstücken wenn der user das volle wort eingibt 
+    #vieleicht hätte man auch parsen können wie ähnlich das wort zum ziel wort ist um schreib fehler 
+    # ab zu fangen man müsste dann aber unter scheiden zwischen keksen und keks einzahl mehrzahl xD 
+
+    if len(eingabe) != 1 or not eingabe.isalpha():
+        print("ungültige eingabe bitte gib genau einen Buchstaben ein.")
+        continue
+    if eingabe in geratene_buchstaben:
+        print("Diesen Buchstaben hast du schon geraten versuch einen anderen")
+        continue
+        
+    geratene_buchstaben.append(eingabe)
+
+
+    if eingabe in geheimes_wort:
+        print(f"gut gemacht {eingabe} ist im wort enthalten")
+        for index, buchstabe in enumerate(geheimes_wort):
+            if buchstabe == eingabe:
+                anzeige[index] = eingabe
+    else:
+        print(f"leider falsch {eingabe} ist nicht im wort")
+        aktuelle_fehler +=1
+
+print("\n ---Spielende---")
+if "-" not in anzeige:
+    print(f"du hast gewonnen das wort {geheimes_wort.upper()} erraten!")
+else:
+    print("leider verloren")
+    print(f"das gesuchte wort war {geheimes_wort.upper()}")
+
